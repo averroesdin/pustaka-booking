1f9d17f5862e52dcb3d0848946262005b3f88d15
averroesdin <mirz4oki@gmail.com>
2021-10-22 20:58:39 +0700

    PERTEMUAN 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,8 +1,20 @@
-<?php
-class Latihan1 extends CI_Controller
-{
- public function index()
- {
- echo "Selamat Datang.. selamat belajar Web Programming";
- }
+<?php 
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<b><h2>Selamat Datang.. selamat belajar Web Programming</h2><cb>"; 
+     
+    } 
+    public function penjumlahan($n1,$n2) 
+    { 
+        /*$this->load->model('Model_Latihan1'); 
+        $hasil = $this->Model_Latihan1->jumlah($n1, $n2); 
+        echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil;
+        */
+        
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "Hasil penjumlahan dari". $n1 ." + ". $n2 ." = ".$hasil;
+    }
 }
